9c0e75fb5cca41596ee3d7f7ec113d1a575d7c5b
Rock S. Artist <rockstarartist@gmail.com>
2015-08-16 00:13:30 -0400

    Removed less, included SASS in its place.

--- assembly/gulp/styles.js
+++ assembly/gulp/styles.js
@@ -1,11 +1,10 @@
 /**
- * Less & CSS compilation
+ * SASS & CSS compilation
  */
 
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    less = require('gulp-less'),
-    //sass = require('gulp-sass'),
+    sass = require('gulp-sass'),
     cssmin = require('gulp-minify-css'),
     concat = require('gulp-concat'),
     source = require('vinyl-source-stream'),
@@ -13,7 +12,7 @@ var gulp = require('gulp'),
     
 gulp.task('styles', function() {
   return gulp.src(package.paths.less)
-  .pipe(less())
+  .pipe(sass())
   .pipe(concat(package.dest.style))
   .pipe(gulp.dest(package.dest.dist))
   .on('error', gutil.log);
@@ -21,16 +20,9 @@ gulp.task('styles', function() {
 
 .task('styles:min', function() {
   return gulp.src(package.paths.less)
-  .pipe(less())
+  .pipe(sass())
   .pipe(concat(package.dest.style))
   .pipe(cssmin())
   .pipe(gulp.dest(package.dest.dist))
   .on('error', gutil.log);
 });
-
-//.task('minifyCss', ['sass'], function() {
-//  return gulp.src(config.cssSrc)
-//    .pipe(minifyCSS({keepBreaks:true}))
-//    .pipe(gulp.dest(config.dest))
-//    .pipe(size());
-//});
